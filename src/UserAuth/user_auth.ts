117638e38f7d94ae62eed8371962cfcbbae16d49
Yuri561 <hazelirvin@gmail.com>
2025-03-31 16:03:02 -0400

    added cors origin

--- src/UserAuth/user_auth.ts
+++ src/UserAuth/user_auth.ts
@@ -2,22 +2,20 @@ import axios from "axios";
 
 const API_URL: string = "https://casapro-backend-o0k1.onrender.com";
 
-
+// User Registration
 export const userRegister = async (userData: any) => {
   return axios.post(`${API_URL}/register`, userData, {
     headers: {
       "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
     },
   });
 };
 
-
+// User Login
 export const userLogin = async (formData: any) => {
   return await axios.post(`${API_URL}/login`, formData, {
     headers: {
       "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
     },
   });
 };
--- src/components/CreateAccount/CreateAccount.tsx
+++ src/components/CreateAccount/CreateAccount.tsx
@@ -34,6 +34,7 @@ const CreateAccount: React.FC = () => {
                 Create Your Casa Pro Account
             </h2>
             <form
+                id="login"
                 onSubmit={handleSubmit}
                 className="w-96 bg-white p-6 rounded-lg shadow-lg">
 
@@ -55,7 +56,7 @@ const CreateAccount: React.FC = () => {
                     placeholder="Password"
                     className="w-full mb-4 px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-teal-500 focus:outline-none"
                 />
-                <button className="w-full bg-teal-500 text-white px-4 py-3 rounded-lg hover:bg-teal-600 transition">
+                <button className="w-full cursor-pointer bg-teal-500 text-white px-4 py-3 rounded-lg hover:bg-teal-600 transition">
                     Create Account
                 </button>
             </form>
